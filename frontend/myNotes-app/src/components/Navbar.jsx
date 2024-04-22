be7af6f09070c21984d7eb4c0359c1476244c582
carnage1502 <shivamsanjay152@gmail.com>
2024-04-22 13:25:37 +0530

    create routes for login,signup and homepage

---
+++ frontend/myNotes-app/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
+      <h2 className="text-xl font-semibold text-black py-2">My Notes!</h2>
+    </div>
+  );
+};
+
+export default Navbar;
